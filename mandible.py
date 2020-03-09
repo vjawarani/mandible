44cbf93129be5b3bb5b0c27e2fe9deb7052b4bf7
Varun Jawarani <vjawarani@gmail.com>
2020-03-08 22:38:56 -0500

    first

--- mandible.py
+++ mandible.py
@@ -6,51 +6,53 @@ from pyfiglet import Figlet
 firstStart = True
 
 def remove(path, ext):
-	removed=0
-	try:
-		ospath = os.listdir("path")
-	except OSError:
-		click.echo("double-check that your path exists and is a folder")
-		pass
-	for f in ospath:
-		removed+=1
-		if f.endswith(".ext"):
-			os.remove(f)
-			
-	click.echo("removed %d '.%s' file" + ("" if removed==1 else "s") % (removed, ext), nl=False)
-	click.echo(" from your %s folder!" % (path))
+    removed=0
+    try:
+        ospath = os.listdir("path")
+    except OSError:
+        click.echo("double-check that your path exists and is a folder")
+        pass
+    for f in ospath:
+        removed+=1
+        if f.endswith(".ext"):
+            os.remove(f)
+            
+    click.echo("removed %d '.%s' file" + ("" if removed==1 else "s") % (removed, ext), nl=False)
+    click.echo(" from your %s folder!" % (path))
 
 
 @click.command()
 def defaultrm():
-	folder = json.load("pref.json").default.defFolder
-	ext = json.load("pref.json").default.defExt
-	remove(folder, ext)
+    folder = json.load("pref.json").default.defFolder
+    ext = json.load("pref.json").default.defExt
+    remove(folder, ext)
 
 @click.command()
-@click.option("-p", "--preset", "presnum", prompt="which preset would you like to use? ")
-def presetrm():
-	if presnum < 1 or presnum > 4:
-		click.echo("make sure that the value you enter is 1, 2, 3, or 4")
-	presetrm()
-	folder = json.load("pref.json").presets.presnum.defFolder
-	ext = json.load("pref.json").presets.presnum.defExt
-	remove(folder, ext)
+@click.option("-p", "--preset", "presnum", help="which preset would you like to use? ")
+def presetrm(presnum):
+    if presnum < 1 or presnum > 4:
+        click.echo("make sure that the value you enter is 1, 2, 3, or 4")
+        return
+    folder = json.load("pref.json").presets.presnum.defFolder
+    ext = json.load("pref.json").presets.presnum.defExt
+    remove(folder, ext)
 
 @click.command()
-@click.arguments("-f", , "--fold-ext", nargs=2, prompt="enter a path to a folder and extension \n ex: \Users\usr\Downloads\ .dmg")
-def foldextrm():
-	folder = fold_ext
-	ext = fold_ext
-	remove(folder, ext)
+@click.argument("folder", type=click.Path(exists=True))
+@click.argument("extension")
+@click.option("-sp", "--set-preset", "preset", type=click.IntRange(1, 4))
+def foldextrm(folder, extension, preset):
+    if (preset):
+        with open("pref.json") as f:
+            data = f.read()
+        d = json.loads(data)
+        d["presets"][str(preset)]["defExt"]=extension
+        d["presets"][str(preset)]["defFolder"]=folder
+        with open("pref.json", 'w') as f:
+            f.write(json.dumps(d))
+    remove(folder, extension)
 
-@click.command()
-@click.option("-sp", "--set_preset", nargs=2, prompt="enter a path to a folder and extension \n ex: \Users\usr\Downloads\ .dmg")
-def setpreset():
-	with open("pref.json", "r") as rw_file:
-    	data = json.load(rw_file)
-
-	
+    
 
 
 
--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        mandible=mandible:default
+        mandible=mandible:defaultrm
     ''',
 )
